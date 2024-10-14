c51ae245e407730476561c73b40c645148b2f217
Pedro Stanzani <pedrohstanzani@gmail.com>
2024-10-14 09:33:10 -0300

    Update docs

--- app/public/_next/static/yBEXgrYS4BRmeyOmNz1b9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-42c4a56efdd6b819.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/_meta":["static/chunks/pages/_meta-b47e7a01bb6b7929.js"],sortedPages:["/","/_app","/_error","/_meta"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
